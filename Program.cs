801b2bb8ed62b7648dca87f2a741b6c3df5b7e5f
Nikita <cherenkov204@gmail.com>
2022-05-09 14:42:55 +0300

    array initialization

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа
+
+Console.WriteLine("Введите элементы массива, разделив их пробелом");
+string InitString = Console.ReadLine();
+string[] array = InitString.Split(" ");
+for (int i = 0; i < array.Length; i++) {
+    Console.WriteLine($"{array.Length} {array[i]}");
+}
